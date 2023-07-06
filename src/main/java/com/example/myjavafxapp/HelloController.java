9ae2d4666ea83b87f40c20d0d9d5d941d412159d
ia-usgs <irvin.i.avitia@gmail.com>
2023-07-06 08:49:50 -0700

    fixed a bunch of stuff, now gotta do the math part

--- src/main/java/com/example/myjavafxapp/HelloController.java
+++ src/main/java/com/example/myjavafxapp/HelloController.java
@@ -2,24 +2,28 @@ package com.example.myjavafxapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.util.Scanner;
 
 public class HelloController {
 
     @FXML
+    private TextField BMITextfield1; //textfield
+
+    @FXML
+    private TextField BMITextfield2; //textfield
+
+    //main button
+    @FXML
     private Label BMIMessageLabel;
 
     @FXML
     private Label BMIMessageLabel2;
 
-    //main button
-    @FXML
-    private Label BMICalcLabel;
-
     @FXML
     protected void onHelloButtonClick() {
-        BMICalcLabel.setText("testing" ); // Change to do the math between label 1 and 2
+        BMIMessageLabel.setText("testing" ); // Change to do the math between label 1 and 2
     }
 }
 
